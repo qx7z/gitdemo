8d1a7282ea6cdd4cf81b5687d195e3e445cab7ec
zqx7 <zqx@mail.ustc.edu.cn>
2024-03-31 13:33:26 +0800

    third commit

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         //System.out.println("first version");
-        System.out.println("second version");
+        //System.out.println("second version");
+        System.out.println("third version");
     }
 }
