60e0a20a4438817d3c7ae781b9d604738cffbc19
zqx7 <zqx@mail.ustc.edu.cn>
2024-03-31 13:47:21 +0800

    hot-fix first commit

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,7 @@ public class Application {
     public static void main(String[] args) {
         //System.out.println("first version");
         //System.out.println("second version");
-        System.out.println("third version");
+        //System.out.println("third version");
+        System.out.println("hot-fix first version");
     }
 }
